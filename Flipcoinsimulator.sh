4e5c815bef81bc2a68faf0e1a1a0627a1338f639
Ankitapatil <ankitakhairnar10@gmail.com>
2021-03-02 21:23:23 +0530

    Added: Won 21 Times

--- Flipcoinsimulator.sh
+++ Flipcoinsimulator.sh
@@ -2,16 +2,35 @@
 count=0
 H=0
 T=0
-while [ $count  -lt  10 ]   
+while [ $count  -lt  42 ]   
 do
-    flipCoin=$(( RANDOM%2 ))
-    if [ $flipCoin -eq  1 ]
-    then
-        H=$(($H+1))
-    else
+       flipCoin=$(( RANDOM%2 ))
+ if [ $flipCoin -eq  1 ]
+ then
+            H=$(($H+1))
+       if [ $H -eq 21 ]
+       then
+              echo "Heads won 21 times"
+      break    
+  fi
+else
         T=$(($T+1))     
-     fi
+       if [ $T -eq 21 ]
+       then
+               echo "Tails won 21 times"
+       break
+      fi
+fi
+
       ((count++))
 done
- echo "head: " $H
- echo "Tail: " $T
+              echo "head won " $H Times
+              echo "Tail won " $T Times
+       if [ $H -gt $T ]
+       then
+             won=$(($H-$T))
+             echo "Heads won by: " $won
+       else
+              won=$(($T-$H))
+              echo "Tails won by: " $won
+        fi
