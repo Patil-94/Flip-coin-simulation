23cda31635a987d2647da883ce371ac25f9f1ceb
Ankitapatil <ankitakhairnar10@gmail.com>
2021-03-02 20:10:29 +0530

    Added: multiple times flipping

--- Flipcoinsimulator.sh
+++ Flipcoinsimulator.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -x
-
-
-flipCoin=$((RANDOM%2))
-if  [$flipCoin -eq 1 ]
-then 
-    echo Head
-else 
-     echo Tail
-fi
+count=0
+H=0
+T=0
+while [ $count  -lt  10 ]   
+do
+    flipCoin=$(( RANDOM%2 ))
+    if [ $flipCoin -eq  1 ]
+    then
+        H=$(($H+1))
+    else
+        T=$(($T+1))     
+     fi
+      ((count++))
+done
+ echo "head: " $H
+ echo "Tail: " $T
